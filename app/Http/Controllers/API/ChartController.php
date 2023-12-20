7326d4c9a5e5270a64ff06d5fc3c837d5f87d042
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2023-12-20 14:55:08 +0800

    Modify Chart Controller get create and update of chart related models

--- app/Http/Controllers/API/ChartController.php
+++ app/Http/Controllers/API/ChartController.php
@@ -14,6 +14,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
+
 class ChartController extends Controller
 {
     public function __construct()
@@ -115,5 +116,165 @@ class ChartController extends Controller
         ];
 
         return response()->json(['data' => $chart, 'message' => 'Patient Chart Created Successfully.'], 201);
+    }
+
+    public function updatePastHistory(Request $request, $id)
+    {
+        $past_history = PastHistory::findOrFail($id)->update([
+            'unremarkable' => $request->unremarkable,
+            'blood_disease' => $request->blood_disease,
+            'asthma' => $request->asthma,
+            'hypertension' => $request->hypertension,
+            'cva' => $request->cva,
+            'gut_disease' => $request->gut_disease,
+            'git_disease' => $request->git_disease,
+            'pulmo_disease' => $request->pulmo_disease,
+            'previous_or' => strtoupper($request->previous_or),
+            'previous_hospitalization' => strtoupper($request->previous_or),
+            'other_findings' => strtoupper($request->previous_or),
+        ]);
+
+        return response()->json(['data' => $past_history, 'message' => 'Past History Record Updated Successfully.'], 200);
+    }
+
+    public function updateFamilyHistory(Request $request, $id)
+    {
+        $family_history = FamilyHistory::findOrFail($id)->update([
+            'unremarkable' => $request->f_unremarkable,
+            'hcvd' => $request->hcvd,
+            'chd' => $request->chd,
+            'cva' => $request->f_cva,
+            'gut_disease' => $request->f_gut_disease,
+            'blood_dyscrasia' => $request->blood_dyscrasia,
+            'allergy' => $request->allergy,
+            'dm' => $request->dm,
+            'git_disease' => $request->f_git_disease,
+            'pulmo_disease' => $request->f_pulmo_disease,
+            'ca' => $request->ca,
+            'other_findings' => strtoupper($request->f_other_findings),
+        ]);
+
+        return response()->json(['data' => $family_history, 'message' => 'Family History Record Updated Successfully'], 200);
+    }
+
+    public function updateSocialHistory(Request $request, $id)
+    {
+        $social_history = SocialHistory::findOrFail($id)->update([
+            'smoking' => strtoupper($request->smoking),
+            'alcohol_intake' => strtoupper($request->alcohol_intake),
+            'betel_nut_chewing' => strtoupper($request->betel_nut_chewing),
+            'drug_food_allergy' => strtoupper($request->drug_food_allergy),
+            'others' => strtoupper($request->others),
+        ]);
+
+        return response()->json(['data' => $social_history, 'message' => 'Social History Record Updated Successfully'], 200);
+    }
+
+    public function updatePatient(Request $request, $id)
+    {
+        $patient = Patient::findOrFail($id)->update([
+            'lname' => strtoupper($request->lname),
+            'fname' => strtoupper($request->fname),
+            'mname' => strtoupper($request->mname),
+            'birthdate' => $request->birthdate,
+            'age' => $request->age,
+            'gender' => $request->gender,
+            'contact_no' => $request->contact_no,
+            'address' => strtoupper($request->address),
+            'vaccination' => strtoupper($request->vaccination),
+            'created_by' => $request->created_by,
+        ]);
+
+        return response()->json(['data' => $patient, 'message' => 'Patient Information Record Updated Successfully'], 200);
+    }
+
+    public function updateConsultation(Request $request, $id)
+    {
+        $consultation_history = ConsultationHistory::findOrFail($id)->update([
+            'physician_id' => $request->physician_id,
+            'consultation_datetime' => $request->consultation_datetime,
+            'payment_type' => $request->payment_type,
+            'chief_complaint' => strtoupper($request->chief_complaint),
+            'subjective' => strtoupper($request->subjective),
+            'objective' => strtoupper($request->objective),
+            'assessment' => strtoupper($request->assessment),
+            'plan' => strtoupper($request->plan),
+            'created_by' => $request->created_by,
+        ]);
+
+        return response()->json(['data' => $consultation_history, 'message' => 'Consultation History Record Updated Successfully'], 200);
+    }
+
+    public function updateVitalSigns(Request $request, $id)
+    {
+        $vital_signs = VitalSigns::findOrFail($id)->update([
+            'height' => $request->height,
+            'weight' => $request->weight,
+            'bmi' => $request->bmi,
+            'bp_f' => $request->bp_f,
+            'bp_s' => $request->bp_s,
+            'oxygen_saturation' => $request->oxygen_saturation,
+            'respiratory_rate' => $request->respiratory_rate,
+            'pulse_rate' => $request->pulse_rate,
+            'cbg' => $request->cbg,
+        ]);
+
+        return response()->json(['data' => $vital_signs, 'message' => 'Vital Signs Record Updated Successfully'], 200);
+    }
+
+    public function createExistingPatient(Request $request, $id)
+    {
+        $consultationNo = ConsultationHistory::generateConsultationNo();
+        $consultation_history = ConsultationHistory::create([
+            'patient_id' => $request->patient_id,
+            'physician_id' => $request->physician_id,
+            'consultation_no' => $consultationNo,
+            'consultation_datetime' => $request->consultation_datetime,
+            'payment_type' => $request->payment_type,
+            'chief_complaint' => strtoupper($request->chief_complaint),
+            'subjective' => strtoupper($request->subjective),
+            'objective' => strtoupper($request->objective),
+            'assessment' => strtoupper($request->assessment),
+            'plan' => strtoupper($request->plan),
+            'created_by' => $request->created_by,
+        ]);
+
+        $vital_signs = VitalSigns::create([
+            'consultation_id' => $consultation_history->id,
+            'height' => $request->height,
+            'weight' => $request->weight,
+            'bmi' => $request->bmi,
+            'bp_f' => $request->bp_f,
+            'bp_s' => $request->bp_s,
+            'oxygen_saturation' => $request->oxygen_saturation,
+            'respiratory_rate' => $request->respiratory_rate,
+            'pulse_rate' => $request->pulse_rate,
+            'cbg' => $request->cbg,
+        ]);
+
+        $chart = [
+            'consultation_history' => $consultation_history,
+            'vital_signs' => $vital_signs,
+        ];
+
+        return response()->json(['data' => $chart, 'message' => 'Consultation Created Successfully'], 201);
+    }
+
+    public function getPast($id)
+    {
+        $past_history = PastHistory::findOrFail($id);
+        return response()->json(['data' => $past_history, 'message' => 'Past History Record Retrieved'], 200);
+    }
+
+    public function getFamily($id)
+    {
+        $family_history = FamilyHistory::findOrFail($id);
+        return response()->json(['data' => $family_history, 'message' => 'Family History Record Retrieved'], 200);
+    }
+
+    public function getSocial($id)
+    {
+        $social_history = SocialHistory::findOrFail($id);
+        return response()->json(['data' => $social_history, 'message' => 'Social History Record Retrieved'], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,20 @@ Route::group(['prefix' => '/auth'], function () {
 Route::group(['prefix' => 'patient'], function () {
     Route::get('/concierge', [PatientController::class, 'concierge']);
     Route::get('/masterfile', [PatientController::class, 'masterfile']);
-    Route::post('/chart/create', [ChartController::class, 'store']);
     Route::patch('/inactive/{id}', [PatientController::class, 'inactive']);
     Route::get('/report', [PatientController::class, 'report']);
+    //chart
+});
+
+Route::group(['prefix' => 'chart'], function () {
+    Route::post('/new/create', [ChartController::class, 'store']);
+    Route::patch('/past/{id}', [ChartController::class, 'updatePastHistory']);
+    Route::patch('/family/{id}', [ChartController::class, 'updateFamilyHistory']);
+    Route::patch('/social/{id}', [ChartController::class, 'updateSocialHistory']);
+    Route::patch('/patient/{id}', [ChartController::class, 'updatePatient']);
+    Route::patch('/consultation/{id}', [ChartController::class, 'updateConsultation']);
+    Route::patch('/vital/{id}', [ChartController::class, 'updateVitalSigns']);
+    Route::post('/old/create', [ChartController::class, 'createExistingPatient']);
 });
 
 Route::group(['prefix' => 'physician'], function () {
