23fef0cc3ee4637cac49304eb7a15a1831f37118
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-01-02 10:24:02 +0800

    Code Clean Up

--- app/Http/Requests/Auth/ForgotPasswordRequest.php
+++ app/Http/Requests/Auth/ForgotPasswordRequest.php
@@ -8,21 +8,12 @@ use Illuminate\Http\Exceptions\HttpResponseException;
 
 class ForgotPasswordRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
+
     public function authorize()
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, mixed>
-     */
     public function rules()
     {
         return [
